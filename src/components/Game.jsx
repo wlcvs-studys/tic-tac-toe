20f0cfd880df254368b88d8468e65cf21c03213f
Wallacy V. da Silva <wlcsv8080@outlook.com>
2026-05-15 01:02:27 -0300

    A button has been added to change the display order of the movements.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ import Board from './Board';
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [order, setOrder] = useState("ascending");
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -17,14 +18,12 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  const moves = history.map((squares, move) => {
+  let moves = history.map((squares, move) => {
     let description;
-
     if (move === 0) {
       description = "Go to game start";
     } else if (move === currentMove) {
         description = "You are at move #" + move;
-
         return (
           <li key={move}>
             <p>{description}</p>
@@ -33,7 +32,6 @@ export default function Game() {
     } else {
       description = "Go to move #" + move;
     }
-
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
@@ -41,12 +39,21 @@ export default function Game() {
     );
   });
 
+  if (order === "descending") {
+    moves = moves.slice().reverse();
+  }
+
+  function changeOrder() {
+      setOrder(order === "ascending" ? "descending" : "ascending");
+  }
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <button onClick={changeOrder}>{order === "ascending" ? "descending" : "ascending"}</button>
         <ul>{moves}</ul>
       </div>
     </div>
